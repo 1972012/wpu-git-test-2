401519e0a8c2742feb129715472483196bd6008a
stefanuskristiyanto <1972012@maranatha.ac.id>
2022-03-04 20:40:42 +0700

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Remote Git</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit illum eos doloremque dicta perferendis voluptatum, aut ullam ex consequuntur, earum tenetur et cumque recusandae voluptate corrupti hic laudantium dolorem blanditiis.</p>
+</body>
+</html>
